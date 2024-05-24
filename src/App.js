99596f96010364bea9c7bd4b131fdadbf0d6ffbc
Minh Dinh <macotontai123@gmail.com>
2024-05-24 17:45:08 +0700

    feat(heading, footer, contact): extract to components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { Layout } from './Layout';
-import { Home } from './pages/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { Layout } from "./Layout";
+import { Home } from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Home />}/>
-            </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }

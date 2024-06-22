3f03293cf1c410fbf7f2225585b4774b7401bf82
tridominh <tridmse173029@fpt.edu.vn>
2024-06-22 09:31:56 +0700

    pay before create post

--- src/pages/student/SchedulePage.jsx
+++ src/pages/student/SchedulePage.jsx
@@ -14,10 +14,22 @@ export function SchedulePage({ token }) {
   const [error, setError] = useState("");
   const [events, setEvents] = useState([]); 
 
+  const days = { 
+      "Sundays": 0, 
+      "Monday": 1, 
+      "Tuesday": 2, 
+      "Wednesday": 3, 
+      "Thursday": 4, 
+      "Friday": 5, 
+      "Saturday": 6
+  };
+
   const fetchSchedules = async (e) => {
     let data = null;
     try {
       data = await GetAllSchedulesOfUser(id);
+      data = data.filter(x => x.booking.status == "DONE");
+      console.log(data);
       setSchedules(data);
       let eventList = [];
       let existingDates = [];
@@ -25,13 +37,17 @@ export function SchedulePage({ token }) {
           //if(!d) continue;
           //console.log(d);
           const tutor = d.booking.bookingUsers?.filter(x => x.role == "Tutor")[0];
+          const slotNum = d.booking.numOfSlots;
 
           const e = generateEvents(
-              formatDate(d.startTime),
-              formatDate(d.endTime),
-              d.booking.subjectLevel.description+
+              moment(),
+              //formatDate(d.endTime),
+              slotNum,
+              d.startTime,
+              d.duration,
+              d.booking.subject.name+" - "+d.booking.level.levelName+
               (tutor!=null ? " with "+tutor?.user.userName : ""),
-              splitDaysOfWeek(d.dayOfWeek),
+              days[d.dayOfWeek],
               existingDates
           );
           eventList.push(...e);
@@ -43,7 +59,7 @@ export function SchedulePage({ token }) {
       setEvents(eventList);
     }
     catch (err) {
-        //console.log(err);
+        console.log(err);
       if (err.response.data.message) {
         // If the error response contains a message, set it as the error message
         setError(err.response.data.message);
@@ -62,17 +78,22 @@ export function SchedulePage({ token }) {
     }
   };
 
-    const generateEvents = (startDate, endDate, subjectName, daysOfWeek, existingDates) => {
-        const start = moment(startDate);
-        const end = moment(endDate);
+    const generateEvents = (startDate, numOfSlot, startTime, duration, subjectName, dayOfWeek, existingDates) => {
+        const start = startDate;
+        //const end = moment(endDate);
         const events = [];
-        //console.log(startDate);
+        let slotsAdded = 0;
+        //console.log(start);
 
-        for (let m = start; m.isSameOrBefore(end); m.add(1, 'days')) {
-            if (daysOfWeek.includes(m.day())) {
+        for (let m = start; slotsAdded < numOfSlot-1; m.add(1, 'days')) {
+            if (dayOfWeek == m.day()) {
+                const startDateTime = moment(m.format('YYYY-MM-DD') + "T" + startTime);
+                const endDateTime = startDateTime.clone().add(moment.duration(duration))
+                //console.log(endDateTime.format('YYYY-MM-DDTHH:mm:ss'))
                 events.push({
                     title: subjectName,
-                    start: m.format('YYYY-MM-DD'),
+                    start: m.format('YYYY-MM-DD')+"T"+startTime,
+                    end: endDateTime.format('YYYY-MM-DDTHH:mm:ss'),
                     allDay: false
                 });
                 if(!existingDates.includes(m.format('YYYY-MM-DD'))){
@@ -82,6 +103,7 @@ export function SchedulePage({ token }) {
                         allDay: true
                     });
                     existingDates.push(m.format('YYYY-MM-DD'));
+                    slotsAdded++;
                 }
             }
         }
@@ -102,6 +124,16 @@ export function SchedulePage({ token }) {
         return daysOfWeek.split(', ').map(Number);
     };
 
+    const filteredEvents = (events) => {
+        return events.filter(event => {
+            var eventDate = moment(event.start); // Assumes `event.start` is in a parsable date format
+            var currentMonthStart = moment().startOf('month');
+            var currentMonthEnd = moment().endOf('month');
+
+            return eventDate.isBetween(currentMonthStart, currentMonthEnd, null, '[]');
+        });
+    };
+
     
 
   useEffect(() => {
@@ -115,11 +147,11 @@ export function SchedulePage({ token }) {
         <FullCalendar
           plugins={[ dayGridPlugin, timeGridPlugin, listPlugin ]}
           initialView="dayGridMonth"
-      headerToolbar={{
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-      }}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+          }}
           views={{ 
               dayGridMonth: { buttonText: 'month' },
               timeGridWeek: { buttonText: 'week' }, 
@@ -127,7 +159,8 @@ export function SchedulePage({ token }) {
               listWeek: { buttonText: 'list' }
           }}
           events={events}
-          dayMaxEvents={4}
+          dayMaxEvents={1}
+          eventDisplay='auto'
         />
     </div>
   )
